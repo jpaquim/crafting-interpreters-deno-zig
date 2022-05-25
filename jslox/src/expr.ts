a7c7c9bda597c915888d6121ef836af1bfe7f2b3
jpaquim <joaopaquim@gmail.com>
2022-05-25 21:47:57 +0100

    Generate visitor implementation for expression classes

--- jslox/src/expr.ts
+++ jslox/src/expr.ts
@@ -1,42 +1,70 @@
 import { LiteralObject, Token } from '../src/token.ts';
 
 export abstract class Expr {
-  static Binary = class Binary extends Expr {
-    constructor(left: Expr, operator: Token, right: Expr) {
-      super();
-      this.left = left;
-      this.operator = operator;
-      this.right = right;
-    }
+  abstract accept<R>(visitor: Visitor<R>): R;
+}
 
-    left: Expr;
-    operator: Token;
-    right: Expr;
-  };
-  static Grouping = class Grouping extends Expr {
-    constructor(expression: Expr) {
-      super();
-      this.expression = expression;
-    }
+export interface Visitor<R> {
+  visitBinaryExpr(expr: Binary): R;
+  visitGroupingExpr(expr: Grouping): R;
+  visitLiteralExpr(expr: Literal): R;
+  visitUnaryExpr(expr: Unary): R;
+}
 
-    expression: Expr;
-  };
-  static Literal = class Literal extends Expr {
-    constructor(value: LiteralObject) {
-      super();
-      this.value = value;
-    }
+export class Binary extends Expr {
+  constructor(left: Expr, operator: Token, right: Expr) {
+    super();
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
 
-    value: LiteralObject;
-  };
-  static Unary = class Unary extends Expr {
-    constructor(operator: Token, right: Expr) {
-      super();
-      this.operator = operator;
-      this.right = right;
-    }
+  override accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitBinaryExpr(this);
+  }
 
-    operator: Token;
-    right: Expr;
-  };
+  left: Expr;
+  operator: Token;
+  right: Expr;
+}
+
+export class Grouping extends Expr {
+  constructor(expression: Expr) {
+    super();
+    this.expression = expression;
+  }
+
+  override accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitGroupingExpr(this);
+  }
+
+  expression: Expr;
+}
+
+export class Literal extends Expr {
+  constructor(value: LiteralObject) {
+    super();
+    this.value = value;
+  }
+
+  override accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitLiteralExpr(this);
+  }
+
+  value: LiteralObject;
+}
+
+export class Unary extends Expr {
+  constructor(operator: Token, right: Expr) {
+    super();
+    this.operator = operator;
+    this.right = right;
+  }
+
+  override accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitUnaryExpr(this);
+  }
+
+  operator: Token;
+  right: Expr;
 }
--- jslox/tool/generate-ast.ts
+++ jslox/tool/generate-ast.ts
@@ -23,16 +23,21 @@ function defineAst(outputDir: string, baseName: string, types: string[]): void {
       `import { LiteralObject, Token } from '../src/token.ts';\n\n`,
     ),
   );
-  writer.write(encoder.encode(`export abstract class ${baseName} {\n`));
+  writer.write(
+    encoder.encode(`export abstract class ${baseName} {
+  abstract accept<R>(visitor: Visitor<R>): R;\n}\n\n`),
+  );
+
+  defineVisitor(writer, baseName, types);
 
   for (const type of types) {
+    writer.write(encoder.encode('\n'));
     const separatorIndex = type.indexOf(':');
     const className = type.slice(0, separatorIndex).trim();
     const fields = type.slice(separatorIndex + 1).trim();
     defineType(writer, baseName, className, fields);
   }
 
-  writer.write(encoder.encode('}\n'));
   writer.close();
 }
 
@@ -45,26 +50,57 @@ function defineType(
   const encoder = new TextEncoder();
 
   writer.write(
-    encoder.encode(
-      `  static ${className} = class ${className} extends ${baseName} {\n`,
-    ),
+    encoder.encode(`export class ${className} extends ${baseName} {\n`),
   );
 
-  writer.write(encoder.encode(`    constructor(${fieldList}) {\n`));
-  writer.write(encoder.encode('      super();\n'));
+  writer.write(encoder.encode(`  constructor(${fieldList}) {\n`));
+  writer.write(encoder.encode('    super();\n'));
 
   const fields = fieldList.split(', ');
   for (const field of fields) {
     const name = field.split(':')[0];
-    writer.write(encoder.encode(`      this.${name} = ${name};\n`));
+    writer.write(encoder.encode(`    this.${name} = ${name};\n`));
   }
 
-  writer.write(encoder.encode('    }\n'));
+  writer.write(encoder.encode('  }\n'));
 
+  // visitor pattern
+  writer.write(encoder.encode('\n'));
+  writer.write(
+    encoder.encode('  override accept<R>(visitor: Visitor<R>): R {\n'),
+  );
+  writer.write(
+    encoder.encode(`    return visitor.visit${className}${baseName}(this);\n`),
+  );
+  writer.write(encoder.encode('  }\n'));
+
+  // fields
   writer.write(encoder.encode('\n'));
   for (const field of fields) {
-    writer.write(encoder.encode(`    ${field};\n`));
+    writer.write(encoder.encode(`  ${field};\n`));
   }
 
-  writer.write(encoder.encode('  };\n'));
+  writer.write(encoder.encode('}\n'));
+}
+
+function defineVisitor(
+  writer: WritableStreamDefaultWriter,
+  baseName: string,
+  types: string[],
+) {
+  const encoder = new TextEncoder();
+
+  writer.write(encoder.encode('export interface Visitor<R> {\n'));
+
+  for (const type of types) {
+    const typeName = type.split(':')[0].trim();
+
+    writer.write(
+      encoder.encode(
+        `  visit${typeName}${baseName}(${baseName.toLowerCase()}: ${typeName}): R;\n`,
+      ),
+    );
+  }
+
+  writer.write(encoder.encode('}\n'));
 }
